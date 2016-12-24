85bbbb1b5c46f1e81c799be2d10877b1783d8595
JP Rodrigues <joaorodrigues@infojr.com.br>
2016-12-24 04:37:13 -0300

    Habilita leitura de Arrays em Prints::it - A função Prints::it agora interpreta datas do tipo 'date/br'

--- helpers/print.php
+++ helpers/print.php
@@ -2,10 +2,40 @@
     class Prints {
 
         /**
-         * 
+         *
          */
-        public static function it($obj, $property, $default = '') {
+        public static function it($obj, $property, $type ='', $default = '') {
+            if (is_array($obj)) (object)$obj;
+            self::switchType($type, $obj, $property);
             echo (!empty($obj->$property))? $obj->$property: $default;
+        }
+
+        /**
+         *
+         */
+        private static function switchType($type, &$obj, $property) {
+            if (!empty($type) && !empty($obj->$property)) {
+                $exp = explode('/', $type);
+                $func = 'a'.ucfirst($exp[0]);
+                $obj->$property = self::$func($obj->$property, $exp[1]);
+            }
+        }
+
+        /**
+         *
+         */
+        public static function aDate($notStoned, $language) {
+            $language = strtolower($language);
+            $date = date_create_from_format('Y-m-d', $notStoned);
+            switch ($language) {
+                case 'br':
+                    return date($date->format('d/m/Y'));
+                    break;
+
+                default:
+                    return $date;
+                    break;
+            }
         }
 
     }
