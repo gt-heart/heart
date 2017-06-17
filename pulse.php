d3d3121029de213ec33d24de924c1fb52bc9552d
Matheus Silva Freitas <msformigteen@live.com>
2017-06-17 12:05:15 -0300

    Namespaces Corrigido

--- pulse.php
+++ pulse.php
@@ -5,8 +5,8 @@
     //This Function will find Classes that the Heart can't see in your project. The heart feels bad :(.
     
     spl_autoload_register( function($className) { 
-        $blood = lAtrium::getDieBlood();
-        $bloodWater = lAtrium::getArterialBlood();
+        $blood = \Heart\lAtrium::getDieBlood();
+        $bloodWater = \Heart\lAtrium::getArterialBlood();
         if ( array_key_exists( $className, $blood) && $bloodWater['autoLoad'] ) { 
             require_once $blood[$className];
             return true; 
